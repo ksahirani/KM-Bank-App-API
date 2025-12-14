151cd5b375b8014414971474923b6b13163061a9
ksahirani <kenonsahirani07@gmail.com>
2025-12-14 16:56:14 +0800

    Added the security for CustomUserDetails and CustomerUserDetailsService

---
+++ km-bank-api/src/main/java/com/kmbank/security/CustomerUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.kmbank.security;
+
+import com.kmbank.entity.User;
+import com.kmbank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
