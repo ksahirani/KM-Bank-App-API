d1bf72949a882eb04dd5c450b59f30a01efccafb
ksahirani <kenonsahirani07@gmail.com>
2025-12-22 23:51:56 +0800

    Added changes in SecurityConfig

--- km-bank-api/src/main/java/com/kmbank/config/SecurityConfig.java
+++ km-bank-api/src/main/java/com/kmbank/config/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -87,4 +89,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
